493ebd0e51296b2b534e0421cccec968b3e2a8ee
jordantangy@gmail.com <jordantangy@gmail.com>
2021-05-25 12:49:13 +0300

    first commit

---
+++ Test.cpp
@@ -0,0 +1,86 @@
+#include "doctest.h"
+#include "BinaryTree.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace ariel;
+using namespace std;
+
+TEST_CASE("create tree and add elements"){
+
+    BinaryTree<string> bt;
+    CHECK_NOTHROW(bt.add_root("4"));
+    CHECK_NOTHROW(bt.add_left("4","3"));
+    CHECK_NOTHROW(bt.add_right("4","5"));
+    CHECK_NOTHROW(bt.add_right("5","7"));
+    CHECK_NOTHROW(bt.add_left("7","10"));
+    string root = "4";
+    auto it = bt.begin_preorder();
+    CHECK(*it == root);
+    it++;
+    CHECK(*it == "3");
+    it++;
+    CHECK(*it == "5");
+    it++;
+    CHECK(*it == "7");
+    it++;
+    CHECK(*it == "10");
+
+    CHECK_NOTHROW(bt.add_root("2"));
+
+}
+TEST_CASE("post order traversal"){
+
+    BinaryTree<string> bt;
+    bt.add_root("4");
+    bt.add_left("4","3");
+    bt.add_right("4","5");
+    bt.add_right("5","7");
+    bt.add_left("7","10");
+
+    vector<string> v = {"3" , "10" , "7" , "5" , "4"};
+    auto it = bt.begin_preorder();
+    for(auto s : v){
+        CHECK(s == *it);
+        it++;
+    }
+
+}
+
+
+
+TEST_CASE("pre order traversal"){
+
+    BinaryTree<string> bt;
+    bt.add_root("4");
+    bt.add_left("4","3");
+    bt.add_right("4","5");
+    bt.add_right("5","7");
+    bt.add_left("7","10");
+
+    vector<string> v = {"4" , "3" , "5" , "7" , "10"};
+    auto it = bt.begin_preorder();
+    for(auto s : v){
+        CHECK(s == *it);
+        it++;
+    }
+
+}
+
+TEST_CASE("in order traversal"){
+
+    BinaryTree<string> bt;
+    bt.add_root("4");
+    bt.add_left("4","3");
+    bt.add_right("4","5");
+    bt.add_right("5","7");
+    bt.add_left("7","10");
+
+    vector<string> v = {"3" , "4" , "5" , "7" , "10"};
+    auto it = bt.begin_inorder();
+    for(auto s : v){
+        CHECK(s == *it);
+        it++;
+    }
+
+}
---
+++ sources/BinaryTree.hpp
@@ -0,0 +1,124 @@
+
+#pragma once
+#include <stdexcept>
+using namespace std;
+
+namespace ariel{
+    template<typename T> class BinaryTree{
+
+        private:
+        struct node{
+            T val;
+            node* right;
+            node* left;
+            node(const T& val):val(val),right(nullptr),left(nullptr){}
+        };
+        node* root;
+
+        
+        class iterator {
+   
+        private:
+        node* ptr= nullptr;
+
+        public:
+        //initialize iterator
+        iterator(node* ptr){
+            this->ptr = ptr;
+        }
+        //operator* returns the value of type T inside the node
+        T& operator*(){
+            T& value = ptr->val;
+             return value;
+        }
+        //operator-> return a pointer of type T to the value of a node
+        T* operator->(){
+         T* ans = &(ptr->val);
+         return ans;
+        }
+        //it++
+        iterator& operator++(){
+           return *this;
+        }
+        //++it
+        iterator operator++(int){
+            return *this;
+        }
+        //checks whether the iterator reached the end of the pointer of the last node of the tree
+        bool operator!=(const iterator& it) {
+            if(ptr != it.ptr){
+                return true;
+            }
+           return false;
+        }
+
+
+    };
+        
+        public:
+        BinaryTree<T>():root(nullptr){}
+
+        BinaryTree<T>& add_root(const T& val){
+            node* r = new node(val);
+            root = r;
+            return *this;
+        }
+        bool exist(const T& elem){
+            return true;
+        }
+        BinaryTree<T>& add_left(const T& current,const T& left_child){
+            if(!exist(current)){
+                throw std::invalid_argument("the specified node those not exist");
+            }
+            return *this;
+        }
+        BinaryTree<T>& add_right(const T& current,const T& right_child){
+            if(!exist(current)){
+                throw std::invalid_argument("the specified node those not exist");
+            }
+            return *this;
+        }
+        iterator begin_preorder(){
+            iterator it(root);
+            return it;
+        }
+        iterator end_preorder(){
+            iterator it(root);
+            return it;
+        }
+
+        iterator begin_inorder(){
+            iterator it(root);
+            return it;
+        }
+
+        iterator end_inorder(){
+            iterator it(root);
+            return it;
+        }
+        iterator begin_postorder(){
+            iterator it(root);
+            return it;
+        }
+
+        iterator end_postorder(){
+            iterator it(root);
+            return it;
+        } 
+
+        iterator begin(){
+            iterator it(root);
+            return it;
+        }                         
+        iterator end(){
+            iterator it(root);
+            return it;
+        } 
+
+    };
+    
+    template<class T> inline ostream& operator<<(ostream& out, const BinaryTree<T>& val){
+        return out;
+    }
+
+}
